e9f7226834affddcbf8718595e1b7fc1d4c7974f
Patrick Ascanoa <navegadormessi@gmail.com>
2024-05-26 20:43:50 -0400

    initial setup of project

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://in-hermit-82.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ convex/files.ts
@@ -0,0 +1,31 @@
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const createFile = mutation({
+  args: {
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("you must be logged in to upload a file");
+    }
+
+    await ctx.db.insert("files", { name: args.name });
+  },
+});
+
+export const getFiles = query({
+  args: {},
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      return [];
+    }
+
+    const files = await ctx.db.query("files").collect();
+    return files;
+  },
+});
